376cc8a13500c183e25b12a1f6e869fc5306ea30
JubaerHossain <jubaer01.cse@gmail.com>
2023-04-06 23:14:22 +0600

    gosql package initialize

---
+++ Readme.md
@@ -0,0 +1,117 @@
+# GoSQL
+
+[![GoDoc](https://godoc.org/github.com/your-username/gosql?status.svg)](https://godoc.org/github.com/your-username/gosql)
+[![Go Report Card](https://goreportcard.com/badge/github.com/your-username/gosql)](https://goreportcard.com/report/github.com/your-username/gosql)
+
+
+
+GoSQL is a package that provides a simple way to query and manipulate SQL databases from GraphQL resolvers in Go.
+
+Features - Query models by ID or with a where clause. - Specify which fields to select from the database using GraphQL queries. - Automatically convert database rows into Go struct instances. - Easily build a where clause with a map of column names and values. - Supports PostgreSQL, MySQL, and SQLite databases.
+Usage
+To use GoSQL, first, you need to import the package:
+
+```go
+import "github.com/your-username/gosql"
+```
+
+Then, define your model struct and make sure that the field names match the column names in your database table:
+
+````go
+type User struct {
+    ID        int
+    FirstName string
+    LastName  string
+    Email     string
+}```
+You can then use GoSQL to query the database and get the results as a slice of struct instances:
+
+```go
+func GetUser(params graphql.ResolveParams) (interface{}, error) {
+    // Query the database for users
+    users, err := gosql.QueryModel(reflect.TypeOf(User{}), "users", params)
+    if err != nil {
+        return nil, err
+    }
+
+    // Return the results
+    return users, nil
+}```
+You can also query a single record by ID:
+
+```go
+func GetUserByID(params graphql.ResolveParams) (interface{}, error) {
+    // Query the database for a user by ID
+    user, err := gosql.FindByID(reflect.TypeOf(User{}), "users", params)
+    if err != nil {
+        return nil, err
+    }
+
+    // Return the result
+    return user, nil
+}```
+Where Clauses
+You can also build a where clause using a map of column names and values:
+
+```go
+func GetUsersWithLastName(params graphql.ResolveParams) (interface{}, error) {
+    // Build the where clause
+    where := make(map[string]interface{})
+    where["last_name"] = params.Args["lastName"].(string)
+
+    // Query the database for users
+    users, err := gosql.QueryModel(reflect.TypeOf(User{}), "users", params, where)
+    if err != nil {
+        return nil, err
+    }
+
+    // Return the results
+    return users, nil
+}```
+Selecting Fields
+You can specify which fields to select from the database using GraphQL queries:
+
+
+```graphql
+{
+    users {
+        firstName
+        email
+    }
+}```
+
+
+```go
+func GetUser(params graphql.ResolveParams) (interface{}, error) {
+    // Get the columns to select from the query
+    selectColumn := gosql.GetColumns(params)
+
+    // Query the database for users
+    users, err := gosql.QueryModel(reflect.TypeOf(User{}), "users", selectColumn, params)
+    if err != nil {
+        return nil, err
+    }
+
+    // Return the results
+    return users, nil
+}```
+Supported Databases
+GoSQL supports PostgreSQL, MySQL, and SQLite databases. To connect to your database, you need to set the DB variable in the database package to a database handle.
+
+```go
+import "database/sql"
+import _ "github.com/lib/pq"
+import "lms/database"
+
+func init() {
+    db, err := sql.Open("postgres", "user=postgres dbname=lms sslmode=disable")
+    if err != nil {
+        panic(err)
+    }
+
+    database.DB = db
+}```
+Contributing
+Contributions are welcome! If you find a bug or want to add a feature, please open an issue or submit a pull request.
+
+````
---
+++ gosql.go
@@ -0,0 +1,343 @@
+package gosql
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/graphql/language/ast"
+)
+
+func GetColumns(params graphql.ResolveParams) string {
+	fieldASTs := params.Info.FieldASTs
+	var fields = make(map[string]interface{})
+	for _, val := range fieldASTs {
+		var cols []string
+		for _, sel := range val.SelectionSet.Selections {
+			field, ok := sel.(*ast.Field)
+			if ok {
+				if field.Name.Kind == "Name" {
+					cols = append(cols, field.Name.Value)
+				}
+			}
+		}
+		fields[val.Name.Value] = cols
+	}
+
+	funclabel := fmt.Sprint(params.Info.Path.Key)
+	cols := fields[funclabel].([]string) //
+	selectColumn := strings.Join(cols, ",")
+	return selectColumn
+
+}
+
+func ModelColumn(selectColumn string, v interface{}) ([]interface{}, error) {
+	var columns []interface{}
+	for _, column := range strings.Split(selectColumn, ",") {
+		fieldName := strings.ToTitle(column[:1]) + column[1:]
+		fieldValue := reflect.ValueOf(v).Elem().FieldByName(fieldName)
+		if !fieldValue.IsValid() {
+			return nil, fmt.Errorf("invalid field name: %s", fieldName)
+		}
+		columns = append(columns, fieldValue.Addr().Interface())
+	}
+	if len(columns) == 0 {
+		return nil, errors.New("no columns selected")
+	}
+	return columns, nil
+}
+
+func BuildWhereClause(where map[string]interface{}) (string, []interface{}) {
+	var whereClauses []string
+	var whereArgs []interface{}
+
+	for key, value := range where {
+		whereClauses = append(whereClauses, fmt.Sprintf("%s = ?", key))
+		whereArgs = append(whereArgs, value)
+	}
+	return strings.Join(whereClauses, " AND "), whereArgs
+}
+
+func QueryModel(modelType reflect.Type, modelName string, params graphql.ResolveParams) (interface{}, error) {
+	// Get the database connection
+	db := database.DB
+
+	where, ok := params.Args["where"].(map[string]interface{})
+	if !ok {
+		where = make(map[string]interface{})
+	}
+
+	// Get the query parameters
+	page, ok := params.Args["page"].(int)
+	if !ok {
+		page = 1
+	}
+	pageSize, ok := params.Args["pageSize"].(int)
+	if !ok {
+		pageSize = 10
+	}
+	offset := (page - 1) * pageSize
+
+	// Build the SQL query string
+	selectColumn := GetColumns(params)
+	whereClause, whereArgs := BuildWhereClause(where)
+	if whereClause == "" {
+		whereClause = "1 = 1"
+	}
+	sql := fmt.Sprintf("SELECT %s FROM %s WHERE %s ORDER BY id DESC LIMIT %d OFFSET %d;", selectColumn, modelName, whereClause, pageSize, offset)
+	// Execute the query
+	rows, err := db.Query(sql, whereArgs...)
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+	defer rows.Close()
+
+	// Create a slice to hold the query results
+	results := reflect.MakeSlice(reflect.SliceOf(modelType), 0, pageSize)
+
+	// Loop through the query results and add them to the results slice
+	for rows.Next() {
+		// Create a new model instance
+		model := reflect.New(modelType).Interface()
+
+		// Get a list of pointers to the fields in the model struct
+		columns, err := ModelColumn(selectColumn, model)
+		if err != nil {
+			return nil, err
+		}
+
+		// Scan the current row of data into the model struct fields
+		err = rows.Scan(columns...)
+		if err != nil {
+			return nil, errors.New("no data found")
+		}
+
+		// Add the model to the results slice
+		results = reflect.Append(results, reflect.ValueOf(model).Elem())
+	}
+
+	// Convert the results slice to an interface{} and return it
+	return results.Interface(), nil
+}
+
+func FindByID(modelType reflect.Type, modelName string, params graphql.ResolveParams) (interface{}, error) {
+	// Get the database connection
+	db := database.DB
+
+	// Get the query parameters
+	id, ok := params.Args["id"].(int)
+	if !ok {
+		return nil, errors.New("id is required")
+	}
+	// Build the SQL query string
+	selectColumn := GetColumns(params)
+	sql := fmt.Sprintf("SELECT %s FROM %s WHERE id = %d;", selectColumn, modelName, id)
+	// Execute the query
+	row := db.QueryRow(sql)
+	// Create a new model instance
+	model := reflect.New(modelType).Interface()
+
+	// Get a list of pointers to the fields in the model struct
+	columns, err := ModelColumn(selectColumn, model)
+	if err != nil {
+		return nil, err
+	}
+	// Scan the current row of data into the model struct fields
+	err = row.Scan(columns...)
+	if err != nil {
+		return nil, errors.New("no data found")
+	}
+	return reflect.ValueOf(model).Elem().Interface(), nil
+}
+
+func QueryModelCount(modelName string, params graphql.ResolveParams) (interface{}, error) {
+	// Get the database connection
+	db := database.DB
+
+	// Build the SQL query string
+	sql := fmt.Sprintf("SELECT COUNT(*) FROM %s;", modelName)
+
+	// Execute the query
+	row := db.QueryRow(sql)
+
+	// Create a variable to hold the count
+	var count int
+
+	// Scan the current row of data into the count variable
+	err := row.Scan(&count)
+	if err != nil {
+		return nil, errors.New("no data found")
+	}
+
+	// Convert the count to an interface{} and return it
+	return count, nil
+}
+
+func CreateModel(modelType reflect.Type, modelName string, params graphql.ResolveParams) (interface{}, error) {
+	// Get the database connection
+	db := database.DB
+
+	// Get the model data from the GraphQL params
+	model := params.Args["model"]
+
+	// Convert the model data to a map
+	modelMap, ok := model.(map[string]interface{})
+	if !ok {
+		return nil, errors.New("invalid model")
+	}
+
+	// Create a slice to hold the field names and a slice to hold the field values
+	var fields []string
+	var values []interface{}
+
+	// Loop through the model fields and add them to the fields and values slices
+	for key, value := range modelMap {
+		fields = append(fields, key)
+		values = append(values, value)
+	}
+
+	// Build the SQL query string
+	fieldString := strings.Join(fields, ",")
+	valueString := strings.Repeat("?,", len(fields))
+	valueString = valueString[:len(valueString)-1]
+	sql := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s);", modelName, fieldString, valueString)
+
+	// Execute the query
+	result, err := db.Exec(sql, values...)
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	// Get the number of rows affected by the query
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	if rowsAffected == 0 {
+		return nil, errors.New("failed to create model")
+	}
+
+	// Return the newly created model data
+	return model, nil
+}
+
+func UpdateModel(modelType reflect.Type, modelName string, params graphql.ResolveParams) (interface{}, error) {
+	// Get the database connection
+	db := database.DB
+
+	// Get the model data from the GraphQL params
+	model := params.Args["model"]
+
+	// Convert the model data to a map
+	modelMap, ok := model.(map[string]interface{})
+	if !ok {
+		return nil, errors.New("invalid model")
+	}
+
+	// Create a slice to hold the field names and a slice to hold the field values
+	var fields []string
+	var values []interface{}
+
+	// Loop through the model fields and add them to the fields and values slices
+	for key, value := range modelMap {
+		fields = append(fields, fmt.Sprintf("%s = ?", key))
+		values = append(values, value)
+	}
+
+	// Build the SQL query string
+	fieldString := strings.Join(fields, ",")
+	sql := fmt.Sprintf("UPDATE %s SET %s WHERE id = ?;", modelName, fieldString)
+
+	// Execute the query
+	result, err := db.Exec(sql, values...)
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	// Get the ID of the updated model
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	params.Args["id"] = id
+
+	// Return the updated model
+	return FindByID(modelType, modelName, params)
+}
+
+func DeleteModel(modelType reflect.Type, modelName string, params graphql.ResolveParams) (interface{}, error) {
+	// Get the database connection
+	db := database.DB
+
+	// Get the model ID from the GraphQL params
+	id, ok := params.Args["id"].(int)
+	if !ok {
+		return nil, errors.New("id is required")
+	}
+
+	// Build the SQL query string
+	sql := fmt.Sprintf("DELETE FROM %s WHERE id = ?;", modelName)
+
+	// Execute the query
+	result, err := db.Exec(sql, id)
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	// Get the number of rows affected
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return nil, errors.New(err.Error())
+	}
+
+	// Return the number of rows affected
+	return rows, nil
+}
+
+func WhereModel(modelType reflect.Type, tableName string, params graphql.ResolveParams, where map[string]interface{}) (interface{}, error) {
+	// Get the database connection
+	db := database.DB
+
+	// Build the SQL query string
+	selectColumn := GetColumns(params)
+	whereClause, whereArgs := BuildWhereClause(where)
+	sql := fmt.Sprintf("SELECT %s FROM %s WHERE %s;", selectColumn, tableName, whereClause)
+
+	// Execute the query
+	rows, err := db.Query(sql, whereArgs...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Create a new slice to hold the model instances
+	models := reflect.MakeSlice(reflect.SliceOf(modelType), 0, 0)
+
+	// Loop through the query results and create a new model instance for each row
+	for rows.Next() {
+		// Create a new model instance
+		model := reflect.New(modelType).Interface()
+
+		// Get a list of pointers to the fields in the model struct
+		columns, err := ModelColumn(selectColumn, model)
+		if err != nil {
+			return nil, err
+		}
+
+		// Scan the current row of data into the model struct fields
+		err = rows.Scan(columns...)
+		if err != nil {
+			return nil, errors.New("no data found")
+		}
+
+		// Append the model instance to the models slice
+		models = reflect.Append(models, reflect.ValueOf(model).Elem())
+	}
+
+	// Convert the models slice to an interface{} and return it
+	return models.Interface(), nil
+}
